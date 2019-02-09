d3951b1e10099e0154a8c31a307e4e7c541c475b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-02-09 20:17:36 +0200

    debugging

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
 app.post("/intake-form", (req, res) => {
 	const email = req.body.email;
 	const company = req.body.company;
